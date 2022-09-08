b6d19d8cbd345c1ee4631a19159f7d713cd21b3a
Anand-Thakar <007andythakare@gmail.com>
2022-09-08 15:00:47 -0400

    added print statement and created new class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Test application for Git demo...");
+        System.out.println("creating another class and printing hello world there");
+
     }
 }

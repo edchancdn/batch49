3dbbe8a920925d1ce6916ed4ecb77851d97b644c
edchancdn <edchan.cdn@gmail.com>
2022-09-08 16:00:09 -0400

    added greet method in HelloWorld class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
 
-
     public static void main(String[] args) {
-
         System.out.println("Hello World");
+
+        greet("John Doe");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello " + name);
     }
 
 
